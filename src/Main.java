271b1abcf1cb30d8388b36d48162e057f38af07d
Alina-kio <alinaolkim@gmail.com>
2023-01-05 22:13:31 +0600

    the task of ingenuity

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+// Написать метод в котором генерируется случайный возраст public static int generateRandomAge(){........}
+// При вызове метода, который формирует результат “можно ли идти гулять”
+// использовать генерирование случайного возраста с помощью метода “generateRandomAge” test(23, 10); test(generateRandomAge(), 10);
+
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         System.out.println("");
@@ -10,6 +15,11 @@ public class Main {
         System.out.println(ageAndTemperature(50, 15));
         System.out.println("");
         System.out.println(ageAndTemperature(22, -40));
+
+
+
+        System.out.println("");
+        System.out.println(ageAndTemperature(generateRandomAge(0, 100), 10));
     }
 
     public static String ageAndTemperature(int age, int temperature) {
@@ -37,6 +47,13 @@ public class Main {
 
         String data = "Возраст человека = " + age + " Температура на улице = " + temperature;
         return data;
+    }
+
+
+
+    public static int generateRandomAge(int min, int max){
+        int x = (int) ((Math.random() * (max - min + 1)) + min);   // 101
+        return x;
     }
 }
 
